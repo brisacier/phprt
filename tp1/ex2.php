6388ebfcea5186ae6255032622542d363732a62a
Clement <clement.brisacier@unicaen.fr>
2018-02-20 11:09:08 +0100

    Modif et fin exo2 tp1

--- tp1/ex2.php
+++ tp1/ex2.php
@@ -19,28 +19,39 @@ if ($args==0){
 }
 else{
 	//test si oubli paramètres
-	if(!isset($_GET["color"]){
-		$color="black"
+	if(!isset($_GET["color"])){
+		$color="black";
 	}
-	if(!isset($_GET["size"]){
-		$size=12
+	if(!isset($_GET["size"])){
+		$size=12;
 	}
 	
 	// récupération paramètres
-	$color=$_GET["color"]
-	$size=$_GET["size"]
-	$message=$_GET["message"]
+	$color=$_GET["color"];
+	$size=$_GET["size"];
+	$message=$_GET["message"];
 	
-	echo(<p style="font-size");
+	echo "<div style='font-size:{$size}px; color:{$color};'>{$message}</div>";
 	
 }
 ?>
 
-<div style="display:flex; flex-direction:column;">
+<div style="display:flex; flex-direction:column; justify-content:space-around; height:100px; border: 1px solid lightgrey;">
 <a href="./ex2.php?message=Salut a tous&color=red&size=15"> Afficher bonjour en rouge taille 15 </a>
-<a href="./ex2.php?message=Casse toi, sale français!&color=green&size=30"> Afficher au revoir en vert taille 30 </a>
-
+<a href="./ex2.php?message=Au revoir !&color=green&size=30"> Afficher au revoir en vert taille 30 </a>
 </div>
+
+<form method="GET" action="./ex2.php">
+	<div>
+    <label for="size">Taille : </label>
+    <input type="number" min="0" value="20" name="size" id="size">
+    <label for="color">Couleur : </label>
+    <input type="color" value="" name="color" id="color">
+    </div>
+    <textarea placeholder="Entrez votre message ici..."  rows="5" cols="50" name="message"></textarea>
+
+    <input type="submit" value="Valider">
+</form>
 
 
 </body>
